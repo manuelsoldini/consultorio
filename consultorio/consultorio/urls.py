f9ebacbbfff30f796f373a25827352a0718ca9e9
Manuel Soldini <manuel.soldini@gmail.com>
2016-10-08 18:21:22 -0300

    admin inline

--- consultorio/consultorio/urls.py
+++ consultorio/consultorio/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 
+admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
--- consultorio/fichas/admin.py
+++ consultorio/fichas/admin.py
@@ -2,18 +2,26 @@ from django.contrib import admin
 from related_admin import RelatedFieldAdmin
 from .models import *
 
-# Register your models here.
-class AdminAnimal(RelatedFieldAdmin):
-    search_fields = ['nombre', 'dueño', 'telefono']
-    list_display = ('especie__nombre', 'especie__imagen')
-
-class AdminEntrada(admin.ModelAdmin):
+class AdminFecha(admin.ModelAdmin):
     search_fields = ['fecha']
     date_hierarchy = 'fecha'
 
+class AdminEntrada(admin.TabularInline):
+    model = Entrada
+
+class AdminVacuna(admin.TabularInline):
+    model = Vacuna
+
+class AdminAnimal(RelatedFieldAdmin):
+    search_fields = ['nombre', 'dueño', 'telefono']
+    list_display = ('especie__nombre', 'especie__imagen')
+    inlines = [
+        AdminEntrada, AdminVacuna
+    ]
+
 admin.site.register(Animal, AdminAnimal)
-admin.site.register(Entrada, AdminEntrada)
-admin.site.register(Vacuna, AdminEntrada)
+admin.site.register(Entrada, AdminFecha)
+admin.site.register(Vacuna, AdminFecha)
 
 admin.site.register(Raza)
 admin.site.register(Especie)
